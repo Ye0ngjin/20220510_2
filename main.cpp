42c1d6846cf8cae087ec5c3c2f8d35920557b176
yj <wkgek@naver.com>
2022-05-10 17:47:06 +0900

    장애물 구현

--- main.cpp
+++ main.cpp
@@ -6,97 +6,133 @@ using namespace std;
 
 struct FVector2D
 {
-    int X;
-    int Y;
+	int X;
+	int Y;
 };
+
+char Map[10][10] = {
+	{'#','#','#','#','#','#','#','#','#','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ','#',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ','#',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#','#','#','#','#','#','#','#','#','#'}
+};
+
 
 void SetLocation(FVector2D NewLocation);
 
+void DrawBG(FVector2D StartPosition)
+{
+	for (int Y = 0; Y < 10; ++Y)
+	{
+		for (int X = 0; X < 10; ++X)
+		{
+			FVector2D Temp;
+			Temp.X = X + StartPosition.X;
+			Temp.Y = Y + StartPosition.Y;
+			SetLocation(Temp);
+			cout << Map[Y][X];
+		}
+	}
+
+}
 
 int main()
 {
-    bool bRunning = true;
-    srand((unsigned int)time(0));
-    int BlankX = rand() % 10 + 1;
-    int BlankY = rand() % 10 + 1;
-    FVector2D PlayerPosition;
-    PlayerPosition.X = 1 + BlankX;
-    PlayerPosition.Y = 1 + BlankY;
+	bool bRunning = true;
+	FVector2D PlayerPosition;
+	PlayerPosition.X = 0;
+	PlayerPosition.Y = 0;
 
-    while (bRunning)
-    {
-        int KeyCode = _getch();
+	srand(static_cast<unsigned int>(time(nullptr)));
+	FVector2D StartPosition;
+	StartPosition.X = rand() % 10 + 1;
+	StartPosition.Y = rand() % 10 + 1;
 
-        switch (KeyCode)
-        {
-        case 'w':
-        case 'W':
-            PlayerPosition.Y--;
-            break;
-        case 's':
-        case 'S':
-            PlayerPosition.Y++;
-            break;
-        case 'a':
-        case 'A':
-            PlayerPosition.X--;
-            break;
-        case 'd':
-        case 'D':
-            PlayerPosition.X++;
-            break;
-        case 27:
-            bRunning = false;
-            break;
+	while (bRunning)
+	{
+	re:
+		int KeyCode = _getch();
 
-        }
+		switch (KeyCode)
+		{
+		case 'w':
+		case 'W':
+			PlayerPosition.Y--;
+			break;
+		case 's':
+		case 'S':
+			PlayerPosition.Y++;
+			break;
+		case 'a':
+		case 'A':
+			PlayerPosition.X--;
+			break;
+		case 'd':
+		case 'D':
+			PlayerPosition.X++;
+			break;
+		case 27:
+			bRunning = false;
+			break;
+		}
 
-        PlayerPosition.X = PlayerPosition.X < 1 + BlankX ? 1 + BlankX : PlayerPosition.X;
-        PlayerPosition.Y = PlayerPosition.Y < 1 + BlankY ? 1 + BlankY : PlayerPosition.Y;
-        PlayerPosition.X = PlayerPosition.X > 8 + BlankX ? 8 + BlankX : PlayerPosition.X;
-        PlayerPosition.Y = PlayerPosition.Y > 8 + BlankY ? 8 + BlankY : PlayerPosition.Y;
+		PlayerPosition.X = PlayerPosition.X < 1 ? 1 : PlayerPosition.X;
+		PlayerPosition.Y = PlayerPosition.Y < 1 ? 1 : PlayerPosition.Y;
+		PlayerPosition.X = PlayerPosition.X > 8 ? 8 : PlayerPosition.X;
+		PlayerPosition.Y = PlayerPosition.Y > 8 ? 8 : PlayerPosition.Y;
 
-        system("cls");
+		if (Map[PlayerPosition.Y][PlayerPosition.X] == '#')
+		{
+			switch (KeyCode)
+			{
+			case 'w':
+			case 'W':
+				PlayerPosition.Y++;
+				break;
+			case 's':
+			case 'S':
+				PlayerPosition.Y--;
+				break;
+			case 'a':
+			case 'A':
+				PlayerPosition.X++;
+				break;
+			case 'd':
+			case 'D':
+				PlayerPosition.X--;
+				break;
+			case 27:
+				bRunning = false;
+				break;
+			}
 
-        for (int i = 0; i < BlankY; i++)
-        {
-            cout << endl;
-        }
+			goto re;
+		}
+		system("cls");
 
-        for (int i = 0; i < BlankX; i++)
-        {
-            cout << " ";
-        }
-        cout << "##########" << endl;
+		DrawBG(StartPosition);
+		FVector2D Temp;
+		Temp.X = PlayerPosition.X + StartPosition.X;
+		Temp.Y = PlayerPosition.Y + StartPosition.Y;
+		SetLocation(Temp);
+		cout << "P";
+	}
 
-        for (int i = 0; i < 8; i++)
-        {
-            for (int i = 0; i < BlankX; i++)
-            {
-                cout << " ";
-            }
-            cout << "#        #" << endl;
-        }
 
-        for (int i = 0; i < BlankX; i++)
-        {
-            cout << " ";
-        }
-        cout << "##########" << endl;
-
-        SetLocation(PlayerPosition);
-        cout << "P";
-
-    }
-
-    return 0;
+	return 0;
 }
 
 
 void SetLocation(FVector2D NewLocation)
 {
-    COORD Cur;
-    Cur.X = NewLocation.X;
-    Cur.Y = NewLocation.Y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+	COORD Cur;
+	Cur.X = NewLocation.X;
+	Cur.Y = NewLocation.Y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
 }
